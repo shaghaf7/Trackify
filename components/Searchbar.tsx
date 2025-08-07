be4e15d65d5c41e47101cab6c8ac55cbd2199105
shaghaf7 <shaghafkashif@gmail.com>
2025-08-07 23:36:13 +0530

    added just for amazon

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,17 +1,17 @@
-"use client";
+"use client"
 
-import { scrapeAndStoreProduct } from "@/lib/actions";
-import { FormEvent, useState } from "react";
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import { FormEvent, useState } from 'react'
 
 const isValidAmazonProductURL = (url: string) => {
   try {
     const parsedURL = new URL(url);
     const hostname = parsedURL.hostname;
 
-    if (
-      hostname.includes("amazon.com") ||
-      hostname.includes("amazon.") ||
-      hostname.endsWith("amazon")
+    if(
+      hostname.includes('amazon.com') || 
+      hostname.includes ('amazon.') || 
+      hostname.endsWith('amazon')
     ) {
       return true;
     }
@@ -20,36 +20,22 @@ const isValidAmazonProductURL = (url: string) => {
   }
 
   return false;
-};
-
-const isValidFlipkartProductURL = (url: string) => {
-  try {
-    const parsedURL = new URL(url);
-    const hostname = parsedURL.hostname;
-    if (hostname.includes("flipkart.com")) {
-      return true;
-    }
-  } catch (error) {
-    return false;
-  }
-  return false;
-};
+}
 
 const Searchbar = () => {
-  const [searchPrompt, setSearchPrompt] = useState("");
+  const [searchPrompt, setSearchPrompt] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const isValidAmazonLink = isValidAmazonProductURL(searchPrompt);
-    const isValidFlipkartLink = isValidFlipkartProductURL(searchPrompt);
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
 
-    if (!isValidAmazonLink && !isValidFlipkartLink)
-      return alert("Please provide a valid Amazon/Flipkart link");
+    if(!isValidLink) return alert('Please provide a valid Amazon link')
 
     try {
       setIsLoading(true);
+
       // Scrape the product page
       const product = await scrapeAndStoreProduct(searchPrompt);
     } catch (error) {
@@ -57,11 +43,14 @@ const Searchbar = () => {
     } finally {
       setIsLoading(false);
     }
-  };
+  }
 
   return (
-    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
-      <input
+    <form 
+      className="flex flex-wrap gap-4 mt-12" 
+      onSubmit={handleSubmit}
+    >
+      <input 
         type="text"
         value={searchPrompt}
         onChange={(e) => setSearchPrompt(e.target.value)}
@@ -69,15 +58,15 @@ const Searchbar = () => {
         className="searchbar-input"
       />
 
-      <button
-        type="submit"
+      <button 
+        type="submit" 
         className="searchbar-btn"
-        disabled={searchPrompt === ""}
+        disabled={searchPrompt === ''}
       >
-        {isLoading ? "Searching..." : "Search"}
+        {isLoading ? 'Searching...' : 'Search'}
       </button>
     </form>
-  );
-};
+  )
+}
 
-export default Searchbar;
+export default Searchbar
